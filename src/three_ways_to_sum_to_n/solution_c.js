c972b8655386276aa8e4b3b7aa62fbac307bac49
junjiechoo1995 <64540292+junjiechoo1995@users.noreply.github.com>
2020-04-29 22:34:47 +0800

    Update solution_c.js

--- src/three_ways_to_sum_to_n/solution_c.js
+++ src/three_ways_to_sum_to_n/solution_c.js
@@ -2,7 +2,13 @@
 // Task: Provide 3 unique implementations of the following function.
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
+<!DOCTYPE html>
+<script>
+'use strict';
 
 var sum_to_n = function(n) {
-    // your code here
-};
+  return n * (n + 1) / 2;
+}
+
+alert( sum_to_n(5) );
+</script>

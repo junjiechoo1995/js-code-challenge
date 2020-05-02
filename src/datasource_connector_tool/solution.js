6f8fe2a90dd453e3d9ecc0599938a5bc79b72cac
junjiechoo1995 <64540292+junjiechoo1995@users.noreply.github.com>
2020-05-02 19:05:07 +0800

    Update solution.js

--- src/datasource_connector_tool/solution.js
+++ src/datasource_connector_tool/solution.js
@@ -1,3 +1,12 @@
 // Problem 2
 // Task: Implement a datasource connector to abstract away data retrieval and manipulation from the `ViewControllers`.  
 // Your solution shall use only [Vanilla JavaScript](http://vanilla-js.com).  
+let ds = new Datasource();
+ds.getPrices()
+    .then(prices => {
+        prices.forEach(price => {
+            console.log(`Mid price for ${ price.pair } is ${ price.mid() } ${ price.quote() }.`);
+        });
+    }).catch(error => {
+        console.err(error);
+    });
